e3831a2a946de589b236b2d2b54e1d36c4822e55
Tienson Qin <tiensonqin@gmail.com>
2026-05-10 16:32:02 +0800

    Revert "fix: ios testflight invalid binary"
    This reverts commit 61177d19fe6572b06191c642e3f64528d96e1815.

--- scripts/test/logseq/tasks/test_runner.cljs
+++ scripts/test/logseq/tasks/test_runner.cljs
@@ -2,7 +2,6 @@
   (:require [cljs.test :as test]
             [logseq.tasks.db-graph.create-graph-with-clojure-irc-history-test]
             [logseq.tasks.db-graph.create-graph-with-large-sizes-test]
-            [logseq.tasks.ios-build-config-test]
             [logseq.tasks.lang-test]
             [logseq.tasks.util-test]))
 
@@ -10,7 +9,6 @@
   (let [{:keys [fail error]}
         (test/run-tests 'logseq.tasks.db-graph.create-graph-with-large-sizes-test
                         'logseq.tasks.db-graph.create-graph-with-clojure-irc-history-test
-                        'logseq.tasks.ios-build-config-test
                         'logseq.tasks.lang-test
                         'logseq.tasks.util-test)]
     (when (pos? (+ fail error))
